8bcb14664983858b9acf310e3ba729d4e9024ad8
Aniket <kumaraniket900@gmail.com>
2022-04-20 08:31:00 +0530

    One Sop Added

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -9,6 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("2ND MINI PROJECT HAS STARTED !!");
+		System.out.println("2ND MINI PROJECT HAS STARTED !!");
 	}
 
 }
